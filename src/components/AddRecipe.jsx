5d4a66a156f36551201a20ca8e97f5a096e801b9
ZAINAZHAR303 <zainazhar303@gmail.com>
2025-07-13 22:17:09 +0500

    edit the subpage of website

--- src/components/AddRecipe.jsx
+++ src/components/AddRecipe.jsx
@@ -5,7 +5,7 @@ import { div } from "framer-motion/client";
 export const AddRecipe = ()=>{
     return(
         <div className="h-[300px] w-[300] bg-white shadow-lg rounded-md">
-            <p>this is the Add recipe form</p>
+            <p className="top-4">this is the Add recipe form</p>
         </div>
     )
 }
