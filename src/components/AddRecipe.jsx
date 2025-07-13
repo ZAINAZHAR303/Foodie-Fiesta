f5c37507c172dcb16f5fa6860c0206e3ef208426
ZAINAZHAR303 <zainazhar303@gmail.com>
2025-07-13 22:14:55 +0500

    update the add Recipe Page

--- src/components/AddRecipe.jsx
+++ src/components/AddRecipe.jsx
@@ -4,8 +4,8 @@ import { div } from "framer-motion/client";
 
 export const AddRecipe = ()=>{
     return(
-        <div className="h-[300px] w-[300] bg-white shadow-lg">
-
+        <div className="h-[300px] w-[300] bg-white shadow-lg rounded-md">
+            <p>this is the Add recipe form</p>
         </div>
     )
 }
